444989c8e8185e5116163e5e203389e0fdc30a27
entermix123 <balabanovjordan@gmail.com>
2025-06-11 19:51:40 +0300

    fixed code

--- Section 6 Terraform Concepts - Building Blocks/28. Lab - DataSource in Terraform/createInstance.tf
+++ Section 6 Terraform Concepts - Building Blocks/28. Lab - DataSource in Terraform/createInstance.tf
@@ -1,13 +1,12 @@
+data "aws_availability_zones" "available" {}
 
-data "aws_availability_zones" "avilable" {}
-
-data "aws_ami" "latest-ubuntu" {
+data "aws_ami" "latest_ubuntu" {
   most_recent = true
-  owners = ["616178742231"]
+  owners      = ["099720109477"]
 
   filter {
-    name = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"]
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"] 
   }
 
   filter {
@@ -16,14 +15,12 @@ data "aws_ami" "latest-ubuntu" {
   }
 }
 
-
-resource "aws_instance" "MyFirstInstnace" {
-  ami           = data.aws_ami.latest-ubuntu.id
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.latest_ubuntu.id
   instance_type = "t2.micro"
-  availability_zone = data.aws_availability_zones.avilable.names[1]
+  availability_zone = data.aws_availability_zones.available.names[1]
 
   tags = {
     Name = "custom_instance"
   }
-
 }
